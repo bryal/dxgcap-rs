f0c2f42d63cdd5d139c339987389d9d1e0a3960e
bryal <96.bryal@gmail.com>
2015-03-24 23:23:20 +0100

    Add UniqueCOMPtr.query_interface
    Enables use similar to windows CComQIPtr.
    Also add more stuff to test.

--- src/lib.rs
+++ src/lib.rs
@@ -29,22 +29,33 @@ extern crate libc;
 extern crate winapi;
 #[macro_use(c_mtdcall)]
 extern crate dxgi;
+extern crate d3d11;
 
-use dxgi::{ IUnknownT };
+use libc::c_void;
+use std::mem;
+use std::ptr;
+use winapi::{ HRESULT, IID };
+use dxgi::interfaces::*;
 
 /// A unique pointer to a COM object. Handles refcounting.
 pub struct UniqueCOMPtr<T: IUnknownT> {
 	ptr: *mut T,
 }
-
 impl<T: IUnknownT> UniqueCOMPtr<T> {
 	/// Construct a new unique COM pointer from a pointer to a COM object.
 	/// It is the users responsibility to guarantee that no copies of the pointer exists beforehand 
 	pub unsafe fn from_ptr(ptr: *mut T) -> UniqueCOMPtr<T> {
 		UniqueCOMPtr{ ptr: ptr }
 	}
-}
 
+	pub unsafe fn query_interface<U>(&mut self, interface_identifier: &IID) -> UniqueCOMPtr<U>
+		where U: IUnknownT
+	{
+		let mut interface: *mut c_void = ptr::null_mut();
+		assert_eq!(0, self.QueryInterface(interface_identifier, &mut interface));
+		UniqueCOMPtr::from_ptr(interface as *mut U)
+	}
+}
 impl<T: IUnknownT> std::ops::Deref for UniqueCOMPtr<T> {
 	type Target = T;
 
@@ -52,13 +63,11 @@ impl<T: IUnknownT> std::ops::Deref for UniqueCOMPtr<T> {
 		unsafe { &*self.ptr }
 	}
 }
-
 impl<T: IUnknownT> std::ops::DerefMut for UniqueCOMPtr<T> {
 	fn deref_mut(&mut self) -> &mut T {
 		unsafe { &mut*self.ptr }
 	}
 }
-
 #[unsafe_destructor]
 impl<T: IUnknownT> std::ops::Drop for UniqueCOMPtr<T> {
 	fn drop(&mut self) {
@@ -66,12 +75,33 @@ impl<T: IUnknownT> std::ops::Drop for UniqueCOMPtr<T> {
 	}
 }
 
+fn hr_failed(hr: HRESULT) -> bool {
+	hr < 0
+}
+
+pub fn get_adater_outputs(adapter: &mut IDXGIAdapter1) -> Vec<UniqueCOMPtr<IDXGIOutput>> {
+	(0..).map(|i| {
+			let mut output = ptr::null_mut();
+			if hr_failed(adapter.EnumOutputs(i, &mut output)) {
+				None
+			} else {
+				let mut out_desc = unsafe { mem::zeroed() };
+				unsafe { (*output).GetDesc(&mut out_desc) };
+
+				if out_desc.AttachedToDesktop != 0 {
+					Some(unsafe { UniqueCOMPtr::from_ptr(output) })
+				} else { None } } })
+		.take_while(Option::is_some).map(Option::unwrap)
+		.collect()
+}
+
 #[test]
 fn test() {
-	use std::ptr;
 	use libc::{ c_void };
-	use dxgi::interfaces::*;
-	use dxgi::{ CreateDXGIFactory1, IID_IDXGIFactory1, DXGI_ERROR_NOT_FOUND };
+	use dxgi::{ CreateDXGIFactory1, IID_IDXGIFactory1, IID_IDXGIOutput1,
+		IID_IDXGIDevice1, DXGI_ERROR_NOT_FOUND };
+	use d3d11::{ D3D_DRIVER_TYPE, D3D11_SDK_VERSION, D3D_FEATURE_LEVEL,
+		D3D11CreateDevice, ID3D11Device, ID3D11DeviceContext };
 
 	let mut factory = unsafe {
 		let mut factory: *mut c_void = ptr::null_mut();
@@ -87,13 +117,42 @@ fn test() {
 	let adapters: Vec<_> = (0..).map(|i| {
 			let mut adapter = ptr::null_mut();
 			if factory.EnumAdapters1(i, &mut adapter) != DXGI_ERROR_NOT_FOUND {
-				println!("{}", i);
 				Some(unsafe { UniqueCOMPtr::from_ptr(adapter) })
-			} else { None }
-		})
-		.take_while(|v| if let &None = v { false } else { true })
-		.map(|v| v.unwrap())
+			} else { None } })
+		.take_while(Option::is_some).map(Option::unwrap)
 		.collect();
 
-		
+	for (mut outputs, mut adapter) in adapters.into_iter()
+		.map(|mut adapter| (get_adater_outputs(&mut adapter), adapter))
+		.filter(|&(ref outs, _)| !outs.is_empty())
+	{
+		// Creating device for each adapter that has the output
+		let (mut d3d11_device, device_context) = unsafe {
+			let mut d3d11_device: *mut ID3D11Device = ptr::null_mut();
+			let mut device_context: *mut ID3D11DeviceContext = ptr::null_mut();
+			assert_eq!(0,
+				D3D11CreateDevice(mem::transmute::<&mut IDXGIAdapter1, _>(&mut adapter),
+					D3D_DRIVER_TYPE::D3D_DRIVER_TYPE_UNKNOWN,
+					ptr::null_mut(), 0, ptr::null_mut(), 0,
+					D3D11_SDK_VERSION,
+					&mut d3d11_device,
+					&mut D3D_FEATURE_LEVEL::D3D_FEATURE_LEVEL_9_1,
+					&mut device_context));
+			(UniqueCOMPtr::from_ptr(d3d11_device as *mut ID3D11Device),
+				UniqueCOMPtr::from_ptr(device_context)) };
+
+		for mut output in outputs.into_iter()
+			.map(|mut o| unsafe { o.query_interface::<IDXGIOutput1>(&IID_IDXGIOutput1) })
+		{
+			let mut dxgi_device = unsafe {
+				d3d11_device.query_interface::<IDXGIDevice1>(&IID_IDXGIDevice1) };
+
+			let duplicated_output = unsafe {
+				let mut duplicated_output: *mut IDXGIOutputDuplication = ptr::null_mut();
+				assert_eq!(0,
+					output.DuplicateOutput(mem::transmute::<&mut IDXGIDevice1, _>(&mut dxgi_device),
+						&mut duplicated_output));
+				UniqueCOMPtr::from_ptr(duplicated_output) };
+		}
+	}
 }
