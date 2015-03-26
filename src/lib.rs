7d2d97145881b44d53a1b1a62e8a5cdf5dd30310
bryal <96.bryal@gmail.com>
2015-03-26 19:19:54 +0100

    Add DXGIManager with some methods
    Not yet added are methods for capturing.
    Also added two wrapper functions for dxgi/d3d11 things:
    create_dxgi_factory_1 & d3d11_create_device

--- src/lib.rs
+++ src/lib.rs
@@ -39,11 +39,11 @@ use libc::c_void;
 use winapi::{ HRESULT, IID, DWORD, RECT, HMONITOR, BOOL };
 use dxgi::constants::*;
 use dxgi::interfaces::*;
-use dxgi::{ DXGI_OUTPUT_DESC };
+use dxgi::{ CreateDXGIFactory1, DXGI_OUTPUT_DESC };
 use d3d11::constants::*;
-use d3d11::core::interfaces::*;
-use d3d11::resource::interfaces::*;
-use d3d11::{ D3D11_USAGE, D3D11_CPU_ACCESS_FLAG };
+use d3d11::interfaces::*;
+use d3d11::{ D3D11_USAGE, D3D11_CPU_ACCESS_FLAG, D3D_DRIVER_TYPE,
+	D3D_FEATURE_LEVEL, D3D11CreateDevice };
 
 #[repr(C)] struct MONITORINFO {
 	cbSize: DWORD,
@@ -104,6 +104,45 @@ unsafe impl<T> Send for UniqueCOMPtr<T> { }
 
 pub fn hr_failed(hr: HRESULT) -> bool { hr < 0 }
 
+fn c_utf16_to_string(chars: &[u16]) -> String {
+	String::from_utf16_lossy(
+		&chars.iter().cloned().take_while(|&b| b != 0).collect::<Vec<_>>()[..])
+}
+
+fn max<T: PartialOrd>(a: T, b: T) -> T { if a > b { a } else { b } }
+
+fn create_dxgi_factory_1() -> UniqueCOMPtr<IDXGIFactory1> {
+	unsafe {
+		let mut factory: *mut c_void = ptr::null_mut();
+
+		let hr = CreateDXGIFactory1(&IID_IDXGIFactory1, &mut factory);
+		if hr_failed(hr) {
+			panic!("Failed to create DXGIFactory1, {:x}", hr)
+		} else {
+			UniqueCOMPtr::from_ptr(factory as *mut IDXGIFactory1) } }
+}
+
+fn d3d11_create_device<T: IDXGIAdapterT>(adapter: &mut T)
+	-> (UniqueCOMPtr<ID3D11Device>, UniqueCOMPtr<ID3D11DeviceContext>)
+{
+	unsafe {
+		let mut d3d11_device: *mut ID3D11Device = ptr::null_mut();
+		let mut device_context: *mut ID3D11DeviceContext = ptr::null_mut();
+
+		let hr = D3D11CreateDevice(mem::transmute(adapter),
+			D3D_DRIVER_TYPE::D3D_DRIVER_TYPE_UNKNOWN,
+			ptr::null_mut(), 0, ptr::null_mut(), 0,
+			D3D11_SDK_VERSION,
+			&mut d3d11_device,
+			&mut D3D_FEATURE_LEVEL::D3D_FEATURE_LEVEL_9_1,
+			&mut device_context);
+		if hr_failed(hr) {
+			panic!("Failed to create d3d11 device and device context, {:x}", hr)
+		} else {
+			(UniqueCOMPtr::from_ptr(d3d11_device as *mut ID3D11Device),
+				UniqueCOMPtr::from_ptr(device_context)) } }
+}
+
 pub fn get_adater_outputs(adapter: &mut IDXGIAdapter1) -> Vec<UniqueCOMPtr<IDXGIOutput>> {
 	(0..).map(|i| {
 			let mut output = ptr::null_mut();
@@ -127,10 +166,11 @@ struct DuplicatedOutput {
 	dxgi_output_dup: UniqueCOMPtr<IDXGIOutputDuplication>,
 }
 impl DuplicatedOutput {
-	fn get_desc(&mut self) -> DXGI_OUTPUT_DESC {
-		let mut desc = unsafe { mem::zeroed() };
-		self.output.GetDesc(&mut desc);
-		desc
+	fn get_desc(&self) -> DXGI_OUTPUT_DESC {
+		unsafe {
+			let mut desc = mem::zeroed();
+			mem::transmute::<_, &mut Self>(self).output.GetDesc(&mut desc);
+			desc }
 	}
 
 	fn get_frame(&mut self, timeout: Duration) -> Result<UniqueCOMPtr<IDXGISurface1>, HRESULT> {
@@ -184,10 +224,10 @@ impl DuplicatedOutput {
 		if hr_failed(hr) { Err(hr) } else { Ok(()) }
 	}
 
-	fn is_primary(&mut self) -> bool {
+	fn is_primary(&self) -> bool {
 		unsafe {
 			let mut output_desc = mem::zeroed();
-			self.output.GetDesc(&mut output_desc);
+			mem::transmute::<_, &mut Self>(self).output.GetDesc(&mut output_desc);
 			let mut monitor_info: MONITORINFO = mem::zeroed();
 			monitor_info.cbSize = mem::size_of::<MONITORINFO>() as u32;
 			 GetMonitorInfoW(output_desc.Monitor, &mut monitor_info);
@@ -197,14 +237,129 @@ impl DuplicatedOutput {
 	}
 }
 
+pub struct DXGIManager {
+	duplicated_output_i: Option<usize>,
+	duplicated_outputs: Vec<DuplicatedOutput>,
+	capture_source: usize,
+	output_rect: RECT,
+	frame_buf: Vec<u8>,
+	timeout_ms: u32,
+}
+impl DXGIManager {
+	fn new(timeout: Duration) -> Result<DXGIManager, &'static str> {
+		let mut manager = DXGIManager{ duplicated_output_i: None,
+			duplicated_outputs: Vec::with_capacity(3),
+			capture_source: 0,
+			output_rect: RECT{ left: 0, top: 0, right: 0, bottom: 0 },
+			frame_buf: Vec::new(),
+			timeout_ms: max(timeout.num_milliseconds(), 0) as u32 };
+
+		match manager.refresh_output() {
+			Ok(_) => Ok(manager),
+			Err(e) => Err(e)
+		}
+	}
+
+	fn set_capture_source(&mut self, cs: usize) {
+		self.capture_source = cs;
+		self.refresh_output().unwrap()
+	}
+
+	fn get_capture_source(&self) -> usize { self.capture_source }
+
+	fn set_timeout(&mut self, t: Duration) { self.timeout_ms = max(t.num_milliseconds(), 0) as u32 }
+
+	fn gather_output_duplications(&mut self) {
+		// clear output duplications
+		self.duplicated_output_i = None;
+		self.duplicated_outputs.clear();
+
+		let mut factory = create_dxgi_factory_1();
+
+		for (outputs, mut adapter) in (0..).map(|i| {
+				let mut adapter = ptr::null_mut();
+				if factory.EnumAdapters1(i, &mut adapter) != DXGI_ERROR_NOT_FOUND {
+					Some(unsafe { UniqueCOMPtr::from_ptr(adapter) })
+				} else {
+					None } })
+			.take_while(Option::is_some).map(Option::unwrap)
+			.map(|mut adapter| (get_adater_outputs(&mut adapter), adapter))
+			.filter(|&(ref outs, _)| !outs.is_empty())
+		{
+			// Creating device for each adapter that has the output
+			let (d3d11_device, device_context) = d3d11_create_device(&mut *adapter);
+
+			let (d3d11_device, output_duplications) = outputs.into_iter()
+				.map(|out| unsafe { out.query_interface::<IDXGIOutput1>(&IID_IDXGIOutput1).unwrap() })
+				.fold((d3d11_device, Vec::new()), |(d3d11_device, mut out_dups), mut output| {
+					let mut dxgi_device = unsafe {
+						d3d11_device.query_interface::<IDXGIDevice1>(&IID_IDXGIDevice1).unwrap() };
+
+					let duplicated_output = unsafe {
+						let mut duplicated_output: *mut IDXGIOutputDuplication = ptr::null_mut();
+						assert_eq!(0,
+							output.DuplicateOutput(
+								mem::transmute::<&mut IDXGIDevice1, _>(&mut dxgi_device),
+								&mut duplicated_output));
+						UniqueCOMPtr::from_ptr(duplicated_output) };
+					out_dups.push((duplicated_output, output));
+					(unsafe { dxgi_device.query_interface::<ID3D11Device>(&IID_ID3D11Device).unwrap() },
+						out_dups)
+				});
+
+			let d3d11_device = Arc::new(Mutex::new(d3d11_device));
+			let device_context = Arc::new(Mutex::new(device_context));
+
+			for duplicated_output in output_duplications.into_iter()
+				.map(|(duplicated_output, output)| {
+					let (d3d11_device, device_context) = (d3d11_device.clone(), device_context.clone());
+
+					DuplicatedOutput { device: d3d11_device,
+						device_context: device_context,
+						output: output,
+						dxgi_output_dup: duplicated_output }
+				})
+			{
+				self.duplicated_outputs.push(duplicated_output);
+			}
+		}
+
+		for dup_out in & self.duplicated_outputs {
+			let desc = dup_out.get_desc();
+			println!("name: {}, coords: {:?}, attached: {:?}, rotation: {}",
+				c_utf16_to_string(&desc.DeviceName),
+				((desc.DesktopCoordinates.left, desc.DesktopCoordinates.top),
+					(desc.DesktopCoordinates.right, desc.DesktopCoordinates.bottom)),
+				desc.AttachedToDesktop, desc.Rotation as usize);
+		}
+	}
+
+	fn refresh_output(&mut self) -> Result<(), &'static str> {
+		self.gather_output_duplications();
+		self.duplicated_output_i = self.active_output_duplication_index();
+		if let None = self.duplicated_output_i {
+			Err("Failed to get out")
+		} else {
+			Ok(()) }
+	}
+
+	fn active_output_duplication_index(&self) -> Option<usize> {
+		if self.capture_source == 0 {
+			self.duplicated_outputs.iter().position(|o| o.is_primary())
+		} else {
+			self.duplicated_outputs.iter()
+				.enumerate()
+				.filter_map(|(i, out)| if out.is_primary() {
+					None
+				} else {
+					Some(i) })
+				.nth(self.capture_source - 1)
+		}
+	}
+}
+
 #[test]
 fn test() {
-	use libc::{ c_void };
-	use dxgi::{ CreateDXGIFactory1, IID_IDXGIFactory1, IID_IDXGIOutput1,
-		IID_IDXGIDevice1, DXGI_ERROR_NOT_FOUND };
-	use d3d11::{ D3D_DRIVER_TYPE, D3D11_SDK_VERSION, D3D_FEATURE_LEVEL,
-		D3D11CreateDevice, ID3D11DeviceContext, IID_ID3D11Device };
-
 	let mut factory = unsafe {
 		let mut factory: *mut c_void = ptr::null_mut();
 		assert_eq!(0, CreateDXGIFactory1(&IID_IDXGIFactory1, &mut factory));
@@ -217,6 +372,8 @@ fn test() {
 			} else { None } })
 		.take_while(Option::is_some).map(Option::unwrap)
 		.collect();
+
+	let mut duplicated_outputs = Vec::with_capacity(4);
 
 	for (outputs, mut adapter) in adapters.into_iter()
 		.map(|mut adapter| (get_adater_outputs(&mut adapter), adapter))
@@ -239,7 +396,7 @@ fn test() {
 
 		let (d3d11_device, output_duplications) = outputs.into_iter()
 			.map(|out| unsafe { out.query_interface::<IDXGIOutput1>(&IID_IDXGIOutput1).unwrap() })
-			.fold((d3d11_device, Vec::new()), |(mut d3d11_device, mut out_dups), mut output| {
+			.fold((d3d11_device, Vec::new()), |(d3d11_device, mut out_dups), mut output| {
 				let mut dxgi_device = unsafe {
 					d3d11_device.query_interface::<IDXGIDevice1>(&IID_IDXGIDevice1).unwrap() };
 
@@ -258,7 +415,7 @@ fn test() {
 		let d3d11_device = Arc::new(Mutex::new(d3d11_device));
 		let device_context = Arc::new(Mutex::new(device_context));
 
-		let duplicated_outputs: Vec<_> = output_duplications.into_iter()
+		for duplicated_output in output_duplications.into_iter()
 			.map(|(duplicated_output, output)| {
 				let (d3d11_device, device_context) = (d3d11_device.clone(), device_context.clone());
 
@@ -267,6 +424,17 @@ fn test() {
 					output: output,
 					dxgi_output_dup: duplicated_output }
 			})
-			.collect();
+		{
+			duplicated_outputs.push(duplicated_output);
+		}
+	}
+
+	for dup_out in &mut duplicated_outputs {
+		let desc = dup_out.get_desc();
+		println!("name: {}, coords: {:?}, attached: {:?}, rotation: {}",
+			c_utf16_to_string(&desc.DeviceName),
+			((desc.DesktopCoordinates.left, desc.DesktopCoordinates.top),
+				(desc.DesktopCoordinates.right, desc.DesktopCoordinates.bottom)),
+			desc.AttachedToDesktop, desc.Rotation as usize);
 	}
 }
